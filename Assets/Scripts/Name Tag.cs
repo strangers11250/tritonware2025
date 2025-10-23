787842401c02b9326c8ad0e7ecca1c10d20575d7
nbgoucsd <nbgo@ucsd.edu>
2025-10-22 23:42:46 -0700

    fixed clip

--- Assets/Scripts/Name Tag.cs
+++ Assets/Scripts/Name Tag.cs
@@ -1,27 +1,31 @@
 using UnityEngine;
 
+
 public class NameTag : MonoBehaviour
 {
-    public GameObject nameTag;
-    public Transform player;
-    public Vector3 offset;
+   public GameObject nameTag;
+   public Transform player;
+   public Vector3 offset;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        nameTag.SetActive(false);
-    }
 
-    public void ShowNameTag()
-    {
-        nameTag.SetActive(true);
-    }
+   // Start is called once before the first execution of Update after the MonoBehaviour is created
+   void Start()
+   {
+       nameTag.SetActive(false);
+   }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (nameTag.activeSelf) {
-        transform.position = player.position + offset;
-        }
-    }
+
+   public void ShowNameTag()
+   {
+       nameTag.SetActive(true);
+   }
+
+
+   // Update is called once per frame
+   void Update()
+   {
+       if (nameTag.activeSelf) {
+       transform.position = player.position + offset;
+       }
+   }
 }
